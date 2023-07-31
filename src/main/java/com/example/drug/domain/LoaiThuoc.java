360a750ff1b77b8b9fe02a62d580ab924fd3a336
Nguyen Chung Phat <120073107+phatnguyenchung@users.noreply.github.com>
2023-07-31 15:48:56 +0700

    add class loai thuoc and thuoc

--- src/main/java/com/example/drug/domain/LoaiThuoc.java
+++ src/main/java/com/example/drug/domain/LoaiThuoc.java
@@ -1,4 +1,25 @@
 package com.example.drug.domain;
 
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Builder
+@Getter
+@Setter
 public class LoaiThuoc {
+    private Long stt;
+    private String maloaithuoc;
+    private String tentheobh;
+    private String sodangky;
+    private String giatribaohiemchitra;
+    private String hoatchat;
+    private String mahoatchat;
+    private String hamluong;
+    private String duongdung;
+    private boolean lastend;
+    private String hangsanxuat;
+    private String nuocsanxuat;
+    private String donvichinh;
+    private String hotrilieu;
 }
--- src/main/java/com/example/drug/domain/Thuoc.java
+++ src/main/java/com/example/drug/domain/Thuoc.java
@@ -1,4 +1,16 @@
 package com.example.drug.domain;
 
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Builder
+@Getter
+@Setter
 public class Thuoc {
+    private Long stt;
+    private String ma;
+    private String tendonvithuoc;
+    private String mota;
+    private String ghichu;
 }
