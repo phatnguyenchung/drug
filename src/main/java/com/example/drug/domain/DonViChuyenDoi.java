d8801b411b864e2572a3ae2d1e2b0a38fc6205af
Nguyen Chung Phat <120073107+phatnguyenchung@users.noreply.github.com>
2023-07-31 16:04:38 +0700

    don vi chuyen doi

--- src/main/java/com/example/drug/domain/DonViChuyenDoi.java
+++ src/main/java/com/example/drug/domain/DonViChuyenDoi.java
@@ -1,4 +1,19 @@
 package com.example.drug.domain;
 
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Builder
+@Getter
+@Setter
 public class DonViChuyenDoi {
+    private Long stt;
+    private String maloaithuoc;
+    private String donvichinh;
+    private String donvichuyendoi;
+    private String tiledonvichinh;
+    private String pheptinh;
+    private String mota;
+    private String ghichu;
 }
